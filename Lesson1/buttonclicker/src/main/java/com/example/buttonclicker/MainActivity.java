ae7a9a2a21840f3c95603cbb59ddadad202d97b4
Hlebushek6 <krepeer006gl@gmail.com>
2026-03-21 18:54:50 +0300

    Lesson 1 Done

--- Lesson1/buttonclicker/src/main/java/com/example/buttonclicker/MainActivity.java
+++ Lesson1/buttonclicker/src/main/java/com/example/buttonclicker/MainActivity.java
@@ -1,6 +1,7 @@
-package ru.mirea.andreyashkingi.control_lesson1;
+package com.example.buttonclicker;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -12,22 +13,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView myTextView = (TextView) findViewById(R.id.textView);
-        Button button = findViewById(R.id.button);
-        CheckBox checkBox =findViewById(R.id.checkBox);
-        checkBox.setChecked(true);
-        myTextView.setText("New text in MIREA");
-        button.setText("MireaButton");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewStudent.setText("Мой номер по списку №3");
+                checkBox.setChecked(true);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+    public void onMyButtonClick(View view) {
+        textViewStudent.setText("Это не я сделал");
+        checkBox.setChecked(false);
     }
 }
